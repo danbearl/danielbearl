ad77cee6f9998236b78149b5e3cd626b5ce91fab
Dan Bearl <dan.bearl@gmail.com>
2013-05-09 18:18:44 -0400

    Setting slug routes for pages

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,9 @@ Danielbearl::Application.routes.draw do
   root to: 'pages#index'
   resources :users, only: [:new, :create]
   resources :user_sessions, only: [:new, :create, :destroy]
+
+  get "/:slug", to: "pages#show", as: :slug
+  get "/:slug/edit", to: "pages#edit", as: :edit_slug
+  put "/:slug", to: "pages#update", as: :slug
+  post "/:slug", to: "pages#destroy", as: :slug
 end
