3d76fd40323b2984cfa17531a80b674eef099a17
Dan Bearl <dan.bearl@gmail.com>
2013-05-14 13:56:49 -0400

    Sort Pages by priority

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130509215855) do
+ActiveRecord::Schema.define(version: 20130514174958) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
