7936bca77ad553d18c40f29555a7a4457a10d89f
Dan Bearl <dan.bearl@gmail.com>
2013-05-14 23:53:29 -0400

    Generate posts model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130514174958) do
+ActiveRecord::Schema.define(version: 20130515022625) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,13 @@ ActiveRecord::Schema.define(version: 20130514174958) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "priority"
+  end
+
+  create_table "posts", force: true do |t|
+    t.string   "title"
+    t.text     "body"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "users", force: true do |t|
