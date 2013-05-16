7c2e603034142ac2aa29f027b7278bced5006772
Dan Bearl <dan.bearl@gmail.com>
2013-05-16 13:18:41 -0400

    Install rspec

---
+++ spec/models/post_spec.rb
@@ -0,0 +1,27 @@
+require "spec_helper"
+
+describe Post do
+  describe "#lead" do
+    it 'returns the first 300 characters of the post body' do
+      body = ''
+      400.times do
+        body << 'a'
+      end
+
+      post = Post.create!(title: "A post", body: body)
+
+      expect(post.lead.length).to eq(300)
+    end
+
+    it 'returns the full body if less than 300 characters' do
+      body = ''
+      200.times do
+        body << 'a'
+      end
+
+      post = Post.create!(title: "A post", body: body)
+
+      expect(post.lead.length).to eq(200)
+      end
+    end
+  end
