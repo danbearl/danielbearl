0f522f5ddea642def745d3105b0adf5a69f6ece8
Dan Bearl <dan.bearl@gmail.com>
2013-05-17 00:34:54 -0400

    Add tags to posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -33,6 +33,6 @@ class PostsController < ApplicationController
   private
 
   def post_params
-    params.require(:post).permit(:title, :body)
+    params.require(:post).permit(:title, :body, :tags_list)
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130515022625) do
+ActiveRecord::Schema.define(version: 20130516182800) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,6 +30,7 @@ ActiveRecord::Schema.define(version: 20130515022625) do
     t.text     "body"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.text     "tags",       array: true
   end
 
   create_table "users", force: true do |t|
