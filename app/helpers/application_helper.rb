30d49d4d1ff142a9cc8ae920dbc295aa58840ddb
Dan Bearl <dan.bearl@gmail.com>
2014-04-04 15:42:34 -0400

    Integrating syntax highlighting

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -21,7 +21,6 @@ module ApplicationHelper
 
   private
 
-
   def active_link_to(text, path, options={})
     if /^#{url_for(path)}/ =~ request.path
       options[:class] = "#{options[:class]} active"
